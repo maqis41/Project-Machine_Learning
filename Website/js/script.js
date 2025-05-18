e48d6176128c13be3d18d48061f2cc0daea2f7d6
maqis41 <144024666+maqis41@users.noreply.github.com>
2025-05-18 19:19:30 +0700

    Add files via upload

---
+++ Website/js/script.js
@@ -0,0 +1,12 @@
+function goToPredict() {
+  window.location.href = "predict.html";
+}
+
+function goHome() {
+  window.location.href = "index.html";
+}
+
+function predict() {
+  alert("Prediksi berhasil dijalankan! (simulasi)");
+  return false; // untuk mencegah reload form
+}
