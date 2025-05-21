1f94ab82585eb530dea1e96f7ff497f96cc2d271
Hytosz <zakisafri30@gmail.com>
2025-05-21 18:40:03 +0700

    memperbaiki

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
 @app.route('/apa')
 def apa():
-    return send_from_directory('.', 'apa.html')
+    return render_template('apa.html')
 
 @app.route('/kontak')
 def kontak():
-    return send_from_directory('.', 'kontak.html')
+    return render_template('kontak.html')
 
 @app.route('/predict')
 def predict():
-    return send_from_directory('.', 'predict.html')
-
-# Optional: agar bisa akses file gambar dan js
-@app.route('/img/<path:filename>')
-def serve_images(filename):
-    return send_from_directory('img', filename)
-
-@app.route('/js/<path:filename>')
-def serve_js(filename):
-    return send_from_directory('js', filename)
+    return render_template('predict.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
